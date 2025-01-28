cfbb9e0e47aafbd9cb0374646bf1a75297772145
42kharuya <haruya.0411.k@gmail.com>
2025-01-28 16:25:09 +0900

    completed app,Rowing

--- Swift_Practice1-2/ViewController.swift
+++ Swift_Practice1-2/ViewController.swift
@@ -8,12 +8,23 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var label: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func dogsButton(_ sender: Any) {
+        label.text = "ワンワン"
+    }
+    
+    @IBAction func catsButton(_ sender: Any) {
+        label.text = "ニャーニャー"
+    }
+    
+    @IBAction func flogsButton(_ sender: Any) {
+        label.text = "ゲロゲロ"
+    }
 }
 
